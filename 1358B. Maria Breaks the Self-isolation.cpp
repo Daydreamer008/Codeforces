d311953139217ea6bdd04219cc194593b5431779
Daydreamer008 <58206481+Daydreamer008@users.noreply.github.com>
2021-04-19 23:29:54 +0530

    Add files via upload

---
+++ 1358B. Maria Breaks the Self-isolation.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define endl "\n"
+#define cendl cout<<endl
+ 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+ 
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+ 
+#define mod 1000000007
+#define inf 1000000000000000001;
+ 
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define asc(c) c.begin(),c.end()
+#define des(c) c.begin(),c.end(),greater<int>()
+
+#define sumOf(c) accumulate(c.begin(),c.end(),0)
+#define maxOf(c) *max_element(c.begin(),c.end())
+#define minOf(c) *min_element(c.begin(),c.end())
+
+#define pb push_back
+ 
+#define eb emplace_back
+#define f first
+#define s second
+
+ 
+void solve() {
+    ll n;
+    cin>>n;
+    vll arr(n);
+    for(int i =0 ; i <n ; ++i) {
+        cin>>arr[i];
+    }
+    sort(asc(arr));
+    ll granies = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] <= i + 1) {
+            cout << i + 2 << '\n';
+            return;
+        }
+    }
+    cout << 1 << '\n';
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t = 1;
+    cin>>t;
+
+    for(ll tc = 1 ; tc <=t ; ++tc) {    
+        solve();
+    }
+    fflush(stdout);
+    return 0;
+}
